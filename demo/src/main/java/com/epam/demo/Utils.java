a5561b74ee621ce2f766e050e8b15248982db663
Shoghik <shoghik.kachatryan@dci-student.org>
2023-12-05 18:09:24 +0100

    fix algorithm

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
        for (String arg : args) {
-           if (StringUtils.isPositiveNumber(arg)) {
+           if (!StringUtils.isPositiveNumber(arg)) {
                return false;
            }
        }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,22 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return org.apache.commons.lang3.StringUtils.isNumeric(str) && Double.parseDouble(str) > 0;
+        if (str == null) {
+            return false;
+        }
+
+        if (str.startsWith("+")) {
+            return isPositiveNumber(str.substring(1));
+        }
+
+        if (str.startsWith("-") || (str.startsWith("0") && !str.startsWith("0."))) {
+            return false;
+        }
+        try {
+            double num = Double.parseDouble(str);
+            return num > 0;
+        } catch (NumberFormatException e) {
+            return false; // Parsing failed, not a valid number
+        }
     }
 }
